d9dc38a8412499e5d09c9927f9a6618cba9a1915
jzmnmiller40 <jzmnmiller40@knights.ucf.edu>
2022-04-27 10:03:34 -0400

    Final Version
    Final has been completed and ready to submit

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+   <style>
+    .error{color: blue;}
+   </style> 
+<title> Jazmin's Web Form</title>
+</head>
+
+<body>
+<form action="results.php" method="post">
+<h1>Welcome </h1>
+<h3> Please Fill Out Form Before continuing </h3>
+Name: <input type="text" name="name">  <br><br>
+Favourite Food: <input type="text" name="food"> <br><br>
+Age: <input type="number" name="age"> <br> <br>
+E-mail: <input type="text" name="email"><br><br>
+
+<input type="submit" name="submit" value="Submit" >
+</form>
+
+
+
+</body>
+</html>
+
+
+
+
+    
---
+++ results.php
@@ -0,0 +1,61 @@
+<?php
+
+//defined variables with empty values 
+$name="";
+$food="";
+$age ="";
+$email ="";
+
+
+function validate_input ($input){
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+
+}
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];    
+    $food = $_POST['food'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+
+}
+
+if (empty($name)) {
+        echo "Name field empty <br>";
+    }
+
+
+if (empty($food)) {
+        echo "Food field empty <br>"; 
+    }
+  
+
+
+if (empty($age)) {
+        echo "Age field empty <br>";
+    }
+  
+
+if (empty($email)) {
+        echo "Email field empty <br>";
+    }
+
+ 
+    
+// results when user has entered information 
+ECHO "<H2> Results from Submission</H2>";
+
+echo "$name";
+echo "<br>";
+ 
+echo "$food";
+echo "<br>";
+
+echo "$age";
+echo "<br>";
+
+echo "$email";
+echo "<br>";
